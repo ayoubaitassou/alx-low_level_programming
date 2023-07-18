8e1049977cddb4c8f9124ce0f01f4937eecc9184
ayoubaitassou <ait.assou.ayoub@gmail.com>
2023-07-18 09:27:15 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,10 +3,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * &c: The character to print
  *
  * Return: on success 1.
- *         on error, -1 is returned. and errno is set appropriately.
+ *         on error, -1 is returned, and errno is set appropriately.
 */
 int _putchar(char c)
 {
