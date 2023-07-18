d73dd0ab04cb86da76b231ab213913c803d53bed
ayoubaitassou <ait.assou.ayoub@gmail.com>
2023-07-18 14:08:22 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,13 +15,14 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar(48)
+		_putchar(48);
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
+
 			/*
 			 * put space if product is a single number
 			 * place the first digit if its two numbers
